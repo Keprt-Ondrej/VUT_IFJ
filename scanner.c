9a4aa5776860fd6fbfd4b3d115c003d63382bbed
Oleg <88681197+trofimleg0@users.noreply.github.com>
2021-10-26 23:09:26 +0200

    Update scanner.c

--- scanner.c
+++ scanner.c
@@ -10,53 +10,89 @@
 
 #define size_of_length 10
 
-// 1. Инициаизация баффера
-// 2. Чтение символа из стдин
-// 2.1 Добавление его в баффер
-// 3. Вывести весь баффер
-
-// 1. Написать конечный автомат
-
 int str_init(String *s) {
-    s->str = (char *) malloc(size_of_length);
-    if(NULL == s->str) {
+    s->string = (char *) malloc(size_of_length);
+    if(NULL == s->string) 
         return INTERNAL_ERROR;
-    }
     s->length = 0;
-    s->str[s->length] = '\0';
+    s->string[s->length+1] = '\0';
     s->alloc_size = size_of_length;
     return 0;
 }
 
 void str_free(String *s) {
-   free(s->str);
+   free(s->string);
 }
 
 void str_clear(String *s) {
     s->length = 0;
-    s->str[s->length] = '\0';
+    s->string[s->length] = '\0';
 }
 
 int str_add_char(String *s, char c) {
 	if (s->length + 1 >= s->alloc_size) {
 		int alloc_new_size = s->length + size_of_length;
-		s->str = (char *) realloc(s->str, alloc_new_size);
-        if(NULL == s->str) {
+		s->string = (char *) realloc(s->string, alloc_new_size);
+        if(NULL == s->string)
            return INTERNAL_ERROR; 
-        }
 		s->alloc_size = alloc_new_size;
 	}
-	s->str[s->length] = c;
-	s->str[s->length++] = '\0';
+	s->string[s->length] = c;
+	s->string[s->length++] = '\0';
 	return 0;
 }    
 
 char str_del_char(String *s) {
-	if (s->length) {
+	if (s->length) 
         return INTERNAL_ERROR;
-    }
-    else {
+    else
         return 0;
-    } 
-	return s->str[s->length--]; // return deleted char
+	return s->string[s->length--]; // return deleted char
+}
+
+Token* create_token() {
+    Token *token = malloc(sizeof(Token));
+    if (NULL == token)
+        return NULL;
+    return token;    
+}
+
+Token* get_token() {
+    Token *token = create_token();
+    String *buffer = NULL;  
+    str_init(buffer);
+
+    FSM_state state = start;
+    char c;
+    // while(true) {
+        switch(state) {
+            c = getc(stdin);
+            case start: 
+                c = getc(stdin);
+                if(c == '\n') 
+                    state = end_of_line;
+                else if(c >= '1' && c <= '9') {
+                    buffer->string[buffer->length++] = c;
+                    state = integer;
+                    printf("%c", c);
+                }    
+                break;
+
+            case end_of_line:
+                state = start;
+                break;
+            default:
+                printf("DEFAULT \n");
+                break;
+            // case integer:
+            //     if(c == '.') {
+            //         state = decimal_point;
+            //     }
+            //     break;
+    
+        }
+    // }
+    token = NULL;    
+    str_free(buffer);
+    return NULL;
 }
