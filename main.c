ca40c35fe2ee2ed1c3f5045e0417cad48f4b0b26
Oleg <88681197+trofimleg0@users.noreply.github.com>
2021-11-15 16:40:19 +0100

    Update main.c

--- main.c
+++ main.c
@@ -12,7 +12,24 @@ int main(){
     //printf("Hello world\n");
     //scanner_hello();
     //symtable_hello();
-    read_token();
-    wordcount();
+    Token *token = read_token();
+    Token_data data = token->data;
+    Token_type type = token->type;
+
+    while(token != NULL) {
+        printf("%d :", type);
+        if(type == 6 || type == 2)
+            printf("%s\n", data.str);
+        else if(type == 4)
+            printf("%d\n", data.type_integer);
+        else if(type == 5)
+            printf("%f\n", data.type_double);  
+        else
+            printf("%d\n", data.key_word);  
+        token = read_token();
+        type = token->type;
+        data = token->data;
+    }
+    //wordcount();
     return 0;
 }
