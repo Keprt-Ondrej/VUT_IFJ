fbdb1be2e17521d1268be6a589875b179f4d5f82
KeprtOndrej <keprt.ondrej@gmail.com>
2021-12-04 01:23:54 +0100

    better meaning

--- ErrLib.h
+++ ErrLib.h
@@ -15,7 +15,7 @@
 #define SYNTAX_ERROR                    2  // Syntax error
 #define SEM_ERROR_REDEFINE_UNDEFINE_VAR 3  // Semantic error - undefined variable or function
 #define SEM_ERROR_ASSIGN_COMMAND        4  // Semantic error - assignment command
-#define SEM_ERROR_TYPE_INCORRECT        5  // Semantic error - incorrect number/parameter types
+#define SEM_ERROR_TYPE_NUMBER_PARAM_RET_INCORRECT        5  // Semantic error - incorrect number/parameter types
 #define SEM_ERROR_TYPE_COMPAT           6  // Semantic error - types uncompatible
 #define SEM_ERROR_OTHER		            7  // Semantic error - other
 #define UNEXP_ZERO_VALUE_ERROR          8  // Startup error when working with an unexpected zero value
